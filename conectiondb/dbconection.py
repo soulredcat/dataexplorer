638df4624662160e75a823f01e913c671295d966
REDcat <arifsofyankusumadiarto@gmail.com>
2024-10-13 18:27:43 +0700

    sql

---
+++ conectiondb/dbconection.py
@@ -0,0 +1,18 @@
+import mysql.connector
+from mysql.connector import Error
+
+def create_connection():
+    """Create a MySQL connection."""
+    try:
+        connection = mysql.connector.connect(
+            host='localhost',
+            database='coinlist',
+            user='root',
+            password='karangkobar'
+        )
+        if connection.is_connected():
+            print("Connected to MySQL database.")
+        return connection
+    except Error as e:
+        print(f"Error: '{e}'")
+        return None
